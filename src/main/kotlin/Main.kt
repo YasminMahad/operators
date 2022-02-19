6b4ccb92e3e82b5c2be0525d7773984bfb68322c
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-02-19 08:58:35 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+ greetings("charity")
+    division(23,5)
+    add(24,16,12,8)
+    interesting("i like eating")
+}
+fun greetings(name: String){
+    println("hello"+name)
+
+}
+fun division(a:Int,b:Int){
+    var result=a%b
+    println(result)
+}
+fun add(a:Int,b:Int,c:Int,d:Int){
+    var result=a+b+c+d
+    println(result)
+}
+
+fun interesting (funny:String){
+    println("i like eating")
+}
+
+
+
+
+
